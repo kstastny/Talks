dddc11e71abb073f2c121d84d941228eebaff76b
Karel Šťastný <stastnyk@cngroup.dk>
2019-03-11 11:26:00 +0100

    added Request-Reply sample

--- messaging-in-distributed-systems/examples_fsharp/fibonacciClient.fsx
+++ messaging-in-distributed-systems/examples_fsharp/fibonacciClient.fsx
@@ -1,6 +1,10 @@
 #r "packages/RabbitMQ.Client/lib/net451/RabbitMQ.Client.dll"
+
+open System
+open System.Text
+open System.Threading.Tasks
+
 open RabbitMQ.Client.Events
-open System.Threading
 open RabbitMQ.Client
 
 module RabbitMqSettings = 
@@ -16,17 +20,10 @@ module RabbitMqSettings =
 module MessagingSettings = 
 
     let FibExchange = "fibonacci"
-    let FibQueueCalculate = "fibonacci.calculate"
     let FibCalculateRoutingKey = "fibonacci.calculate"
     let FibBroadcastExchange = "fibonacci.broadcast"
 
 
-
-
-open System.Text
-
-open RabbitMQ.Client
-open RabbitMQ.Client.Events
 
 let createConnection = 
     let factory = new ConnectionFactory()
@@ -77,7 +74,54 @@ let createFibListener (channel: IModel) =
 
 
 
-//TODO request-reply (simple or "full" implementation?)
+/// Request-Reply example. Note that normally we would also reuse the Channel but for simplicity we always create a new one
+let requestFibAsync (conn: IConnection) (fib: int) : Task<string> =
+    let correlationId = Guid.NewGuid() |> string
+    let taskCompletionSource = TaskCompletionSource<string>()
+
+    let channel = conn.CreateModel ()
+    channel.BasicReturn |> Event.add (fun e -> 
+        match e.ReplyCode with
+        | 312us -> printfn "Message could not be delivered to consumer"
+                   taskCompletionSource.SetResult("ERROR delivering the message") //should be exception obviously, or ROP Error
+        | _ -> printfn "Basic return - %i: %s" e.ReplyCode e.ReplyText
+        )
+
+    //create consumer that will receive the message
+    let consumer = EventingBasicConsumer(channel)
+    consumer.Received
+    |> Event.add (fun e -> 
+        e.Body |> Encoding.UTF8.GetString
+        //|> (fun x -> printfn "Message for correlationId %s: %s" e.BasicProperties.CorrelationId x; x)
+        |> taskCompletionSource.SetResult
+    )
+
+    //start listening on direct reply-to queue, see https://www.rabbitmq.com/direct-reply-to.html
+    channel.BasicConsume("amq.rabbitmq.reply-to", autoAck=true, consumer=consumer) |> ignore
+
+    //send message
+    let requestProperties = channel.CreateBasicProperties();
+    requestProperties.ReplyTo <- "amq.rabbitmq.reply-to";
+    requestProperties.CorrelationId <- correlationId
+
+    let body = fib |> string |> Encoding.UTF8.GetBytes
+    channel.BasicPublish(
+        MessagingSettings.FibExchange,
+        MessagingSettings.FibCalculateRoutingKey,
+        body = body,
+        mandatory = true,
+        basicProperties = requestProperties
+    )    
+
+
+    //simple timeout, using Polly would be better
+    Task.WhenAny(Task.Delay(5000), taskCompletionSource.Task)
+        .ContinueWith(fun (t: Task) -> 
+            if not (System.Object.ReferenceEquals(t, taskCompletionSource.Task)) then 
+                taskCompletionSource.SetResult("Timeout")
+        ) |> ignore
+
+    taskCompletionSource.Task
 
 
 
@@ -89,6 +133,14 @@ let fib = requestFibCommand channel
 createFibListener channel |> ignore
 
 fib 15
+
+let fibAsync fib =
+    let t = requestFibAsync connection fib
+    t.ContinueWith(fun (x: Task<string>) -> printfn "RESULT = %s" x.Result) |> ignore
+    
+    
+fibAsync 1
+
 
 
 channel.Close ()
